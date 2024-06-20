225eddc0aab8377f9a47182597ac696e74c82608
Zayn Hamid <zainhamid982@gmail.com>
2024-06-20 18:29:08 +0500

    bit of changes

--- src/components/Text Box.jsx
+++ src/components/Text Box.jsx
@@ -1,9 +1,8 @@
-import connectDB from "@/lib/connectDB"
-import Message from "@/models/texts"
-
+import connectDB from "@/lib/connectDB";
+import Message from "@/models/texts";
 
 async function create(formData) {
-  'use server'
+  'use server';
   await connectDB();
   await Message.create({
     message: formData.get("message"),
@@ -11,34 +10,29 @@ async function create(formData) {
 }
 
 const getData = async () => {
-  const res = await fetch('https://text-strangers.vercel.app/api/messages')
-  const data = await res.json()
+  const res = await fetch('http://localhost:3000/api/messages');
+  const data = await res.json();
   return data;
 }
 
 const TextBox = async () => {    
-  await connectDB()
+  await connectDB();
 
-  const data = await getData()
-
-
+  const data = await getData();
 
   return (
     <>
       <div className='flex items-center gap-4'>
-        <form action={create} >
-            <input type="text" name="message" className='text-black px-4 py-1 outline-none rounded-xl' placeholder='Message'/>
-            <input type="submit" value={'Submit'} className='border-2 mx-4 px-4 rounded-xl hover:bg-white cursor-pointer hover:text-black'/>
-     
+        <form action={create}>
+          <input type="text" name="message" className='text-black px-4 py-1 outline-none rounded-xl' placeholder='Message' />
+          <input type="submit" value={'Submit'} className='border-2 mx-4 px-4 rounded-xl hover:bg-white cursor-pointer hover:text-black' />
         </form>
-    
-    </div>
-    {data.mesasage.map((val, i) => (
-        <p  key={i}>{val.message}</p>
+      </div>
+      {data.mesasage.map((val, i) => (
+        <p key={i}>{val.message}</p>
       ))}
     </>
-  
-  )
+  );
 }
 
-export default TextBox
+export default TextBox;
