cad87d4af99bf7a3514ec10782d5c0679d2da0cb
Zayn Hamid <zainhamid982@gmail.com>
2024-06-21 19:00:23 +0500

    xyz

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,27 @@
-import connectDB from "@/lib/connectDB"
-import Message from "@/models/texts"
+'use client'
+import { useEffect, useState } from 'react';
 
-const Users =  async () => {
-    await connectDB()
-    const data = await Message.find() 
+const Users = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/messages');
+      const result = await response.json();
+      console.log(result)
+      setData(result.mesasage);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>  {data.map((val, i) => (
+    <div>
+      {data.map((val, i) => (
         <p key={i} className="text-center">{val.message}</p>
-      ))}</div>
-  )
+      ))}
+    </div>
+  );
 }
 
-export default Users
+export default Users;
